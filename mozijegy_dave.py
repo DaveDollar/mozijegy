abe3865342b73ca7ef83b0c263c2cf87512a4dfa
DaveDollar <illes.david.2006@gmail.com>
2024-02-29 09:19:46 +0100

    kezdetek

--- mozijegy_dave.py
+++ mozijegy_dave.py
@@ -1,1 +1,16 @@
 from tkinter import *
+from tkinter import messagebox
+from ttkbootstrap import *
+
+root = Tk()
+root.title('MoziTown')
+root.geometry('1200x600')
+root.config(bg='#09295c')
+root.resizable(False, False)
+root.iconbitmap('./imgs/cinema.ico')
+
+logoimg = PhotoImage(file='./imgs/MoziTown1.png')
+logo = Label(root, image=logoimg, background='#09295c')
+logo.place(relx=0.5, rely=0, anchor=N)
+
+root.mainloop()

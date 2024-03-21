f67fe83e15ee3d028ff32aca01024bd39b3134f7
CsananaBanana <csanadbukkosi2007@gmail.com>
2024-03-21 09:21:27 +0100

    bapngőea

--- mozijegy.py
+++ mozijegy.py
@@ -6,37 +6,28 @@ root.geometry("1200x600")
 root.config(bg="#09295c")
 root.resizable(False,False)
 
-con = sqlite3.connect("mozitown.db")
+con = sqlite3.connect("mozijegy/mozitown.db")
 cur = con.cursor()
+cur.execute('PRAGMA foreign_keys=on;')
 
 try:
-    cur.execute('''CREATE TABLE Mozijegy (
-                Sorszam integer PRIMARY KEY,
-                Erv_datum DATE NOT NULL,
-                Film VARCHAR(40) NOT NULL,
-                Sor integer NOT NULL,
-                Szek integer NOT NULL);''')
+    cur.execute('''CREATE TABLE Termek (
+                Terem_szama INT PRIMARY KEY,
+                Vetitettfilm VARCHAR(40),
+                Kapacitas INT);''')
 except sqlite3.OperationalError or FileExistsError:
     pass
+
 try:
-    cur.execute('''CREATE TABLE Szemely (
-                Szem_szam integer PRIMARY KEY,
-                Eletkor integer NOT NULL,
-                Daik/Nyugdij BOOLEAN);''')
-except sqlite3.OperationalError or FileExistsError:
-    pass
-try:
-    cur.execute('''CREATE TABLE Vesz (
-                VAZON integer PRIMARY KEY,
-                SZID integer NOT NULL,
-                MID integer NOT NULL,
-                AR VARCHAR(6) NOT NULL,
-                V_datum DATE NOT NULL,
-                FOREIGN KEY (SZID) REFERENCES Szemely (Szem_szam)
-                    ON UPDATE CASCADE
-                    ON UPDATE CASCADE
-                FOREIGN KEY (MID) REFERENCES Mozijegy (Sorszam)
-                    ON UPDATE CASCADE
+    cur.execute('''CREATE TABLE Foglalasok (
+                Sorszam INT AUTO_INCREMENT PRIMARY KEY,
+                Kernev TEXT,
+                Veznev TEXT,
+                Teremszam INT,
+                Szekszam INT,
+                CONSTRAINT foglal
+                FOREIGN KEY (Teremszam) REFERENCES Termek (Terem_szama)
+                    ON DELETE CASCADE
                     ON UPDATE CASCADE);''')
 except sqlite3.OperationalError or FileExistsError:
     pass

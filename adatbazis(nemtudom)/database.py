ef37bb6a2c510f84d35c1e6045384392449aedc2
CsananaBanana <csanadbukkosi2007@gmail.com>
2024-05-09 07:46:06 +0200

    adatbázis működik?

--- adatbazis(nemtudom)/database.py
+++ adatbazis(nemtudom)/database.py
@@ -1,35 +1,22 @@
 import sqlite3
 
-
 connection = sqlite3.connect('database1.py')
 cursor = connection.cursor()
 
 def create_database():
     try:
-        cursor.execute("SELECT * FROM Termek")
+        cursor.execute("SELECT * FROM Foglalasok")
         return
     except Exception:
-        termek = """CREATE TABLE Termek (
-            Terem_szama INT PRIMARY KEY,
-            Vetitettfilm VARCHAR(40) NOT NULL,
-            Kapacitas INT NOT NULL );"""
-        cursor.execute(termek)
         foglalasok = """CREATE TABLE Foglalasok (
         Sorszam INT AUTO_INCREMENT PRIMARY KEY,
-        Kernev TEXT NOT NULL,
-        Veznev TEXT NOT NULL,
-        Teremszam INT NOT NULL,
         Szekszam INT NOT NULL,
-        FOREIGN KEY (Teremszam) REFERENCES Termek(Terem_szama) );"""
+        Nap DATE NOT NULL,
+        Ora TIME NOT NULL);"""
         cursor.execute(foglalasok)
 
-def add_terem(tszam: int, nev: str, kap: int):
-    command = f"""INSERT INTO film VALUES ('{tszam}','{nev}','{kap})"""
-    cursor.execute(command)
-    connection.commit()
-
-def add_foglalas(ker: str, vez: str, tszam: int, szek: int):
-    command = f"""INSERT INTO rendezo VALUES (NULL,'{ker}','{vez}','{tszam}','{szek}')"""
+def add_foglalas(Szekszam, Nap, Ora):
+    command = f"""INSERT INTO termek VALUES ('{Szekszam}','{Nap}','{Ora})"""
     cursor.execute(command)
     connection.commit()
 
